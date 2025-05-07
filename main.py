88d3bb751abf825de102aa512bd92e0476632f15
kf5131 <kf5131@icloud.com>
2025-05-07 17:02:41 +0200

    add valid and solve

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ def print_board(board):
                 print(board[i][j])
             else:
                 print(str(board[i][j]) + " ", end="")
-                
-                
+                            
 def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
@@ -34,9 +33,53 @@ def find_empty(board):
                 return (i, j)
     return None
 
+def valid(board, number, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == number and pos[1] != i:
+            return False
 
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == number and pos[0] != i:
+            return False    
 
+    # Check box 
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i     
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+    
+
+def main():
+    print_board(test_board)
+    solve(test_board)
+    print("____________________")
+    print_board(test_board)
 
 
 if __name__ == "__main__":
-    print_board(test_board)
+    main()
